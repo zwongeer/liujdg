482570f871d3bbe3b5187dbf10d59e7accad938b
zwongeer <zwongeer@gmail.com>
2021-09-09 13:04:48 +0800

    deamon

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 cd $SHELL_FOLDER
 mkdir -p build
 cd build
-cmake .. || exit 1
+if [ "$1" == "d" -o "$1" == "debug" -o "$1" == "Debug" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug .. || exit 1
+else
+    cmake -DCMAKE_BUILD_TYPE=Release .. || exit 1
+fi
 make || exit 1
 echo "(root permission required)"
 read -n1 -p "Do you want to install liujdg?[y/n]" answer
--- tools/liujdg.deamon.cxx
+++ tools/liujdg.deamon.cxx
@@ -1,14 +1,74 @@
+#include <memory>
+
+#include <liujdg_version.hpp>
+
+#include <nlohmann/json.hpp>
 #include <restinio/all.hpp>
 
-int main()
-{
-  restinio::run(
-    restinio::on_this_thread()
-      .port(1984)
-      .address("localhost")
-      .request_handler([](auto req) {
-        return req->create_response().set_body("hello liujdg!").done();
-      }));
+namespace rr = restinio::router;
+using router_t = restinio::router::express_router_t<>;
+auto router = std::make_unique<router_t>();
 
-  return 0;
+template <typename RESPONSE>
+RESPONSE init_response(RESPONSE response) {
+    response.append_header("Server", "Potatoes & Apples");
+    response.append_header("Who-is-txdy", "Attack204");
+    response.append_header_date_field()
+        .append_header( "Content-Type", "text/plain; charset=utf-8" );
+    return response;
+}
+
+auto server_handler() {
+    auto router = std::make_unique<router_t>();
+    auto allMethods = restinio::router::none_of_methods(
+        // restinio::http_method_get(),
+        // restinio::http_method_head(),
+        // restinio::http_method_post(),
+        // restinio::http_method_put(),
+        // restinio::http_method_delete(),
+        // restinio::http_method_connect(),
+        // restinio::http_method_options(),
+        // restinio::http_method_trace(),
+        // restinio::http_method_patch()
+    );
+
+    router->add_handler(allMethods, "/version", [](auto req, auto params) {
+        return init_response(req->create_response())
+                .set_body(liujdg::NAME + " " + liujdg::VERSION)
+                .done();
+    });
+
+    router->non_matched_request_handler([](auto req) {
+        return init_response(req->create_response())
+                .set_body("Hello " + liujdg::NAME + "\n" + "You sent:" + req->body())
+                .done();
+    });
+    return router;
+}
+
+int main() {
+    using namespace std::chrono;
+    try {
+        using traits_t = restinio::traits_t<
+                restinio::asio_timer_manager_t,
+                #ifdef NDEBUG
+                restinio::null_logger_t,
+                #else
+                restinio::single_threaded_ostream_logger_t,
+                #endif
+                router_t >;
+        restinio::run(
+            restinio::on_this_thread<traits_t>()
+            .port(1984)
+            .address("localhost")
+            .request_handler(server_handler())
+            // .read_next_http_message_timelimit(10s)
+            // .write_http_response_timelimit(1s)
+            // .handle_request_timeout(1s)
+        );
+    } catch (std::exception& err) {
+        std::cerr << "Error: " << err.what() << std::endl;
+		return 1;
+    }
+    return 0;
 }
